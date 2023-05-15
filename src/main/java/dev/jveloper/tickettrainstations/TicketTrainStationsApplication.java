a13fed15d4c3364e0599a1b84551fc3f7396588b
jveloper <jveloper82@gmail.com>
2023-05-15 22:25:05 +0100

    add application startup class

---
+++ src/main/java/dev/jveloper/tickettrainstations/TicketTrainStationsApplication.java
@@ -0,0 +1,13 @@
+package dev.jveloper.tickettrainstations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketTrainStationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketTrainStationsApplication.class, args);
+    }
+
+}
